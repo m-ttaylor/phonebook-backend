260a66026d37430a639f0f0beb796841f8093b28
m-ttaylor <mttaylor@email.wm.edu>
2022-05-19 11:21:40 -0400

    Add changes for heroku

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = proccess.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
