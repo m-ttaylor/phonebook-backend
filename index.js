c62b16ae16453c33ed0ffcf729cc53e75968973e
m-ttaylor <mttaylor@email.wm.edu>
2022-05-24 10:41:36 -0400

    Add custom validator for numbers

--- index.js
+++ index.js
@@ -90,53 +90,27 @@ app.get('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name must be included'
-    })
-  }
-
-  if (!body.number) {
-    return response.status(400).json({
-      error: 'number must be included'
-    })
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  console.log('Just adding a duplicate person, editing has not been implemented yet')
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
+  }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name must be included'
-    })
-  }
-
-  if (!body.number) {
-    return response.status(400).json({
-      error: 'number must be included'
-    })
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number }, 
+    {new: true, runValidators: true, context: 'query'}
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -163,6 +137,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message})
   }
   next(error)
 }
